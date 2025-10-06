6bd60922931857e1dbcdaa8a4d2709dbf9dd25d8
rohitkaldate <98863427+rohitkaldate@users.noreply.github.com>
2025-10-06 09:58:40 +0530

    Time: 42 ms (37.43%) | Memory: 17.6 MB (68.20%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,5 +1,6 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+       ## .zfill(32) is used to ensure that the string is 32 bit
        s = bin(n)[2:].zfill(32)
        res = s[::-1]
        return int(res,2)
